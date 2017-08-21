b63e4ebe09f8823973ff5dcbd4f3ddc01ca66dac
David Van Der Beek <earlynovrock@gmail.com>
2017-08-21 07:18:07 -0500

    update

--- inst/simple_server.R
+++ inst/simple_server.R
@@ -1,6 +1,11 @@
 library(methods)
 devtools::load_all()
+load(file = "inst/Linear_model.rda")
 
-routes <- source(microserver:::packagefile('inst/routes.R'))$value
+routes <- list(
+    "/ping"        = function(...) "pong"
+  , "/predict" = function(p, q) { list(result = predict(linear.model, newdata = data.frame(x = as.numeric(q$x)))) }
+  , function(...) list(exception = "catch all route")
+)
 cat('listening..\n')
 microserver::run_server(routes, 33399)
